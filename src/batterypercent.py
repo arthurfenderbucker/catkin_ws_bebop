bc43fdff1b3e6438c44a74e27ee14da0ef5084bd
JoseColombini <42770769+JoseColombini@users.noreply.github.com>
2019-09-28 16:17:58 -0300

    Add files via upload

---
+++ src/batterypercent.py
@@ -0,0 +1,39 @@
+'''
+Code made for test commum state massages and validade the capability
+to cacht battery porcentage
+'''
+
+import rospy
+from bebop_msgs.msg import CommonCommonStateBatteryStateChanged
+#import CommonCommonStateBatteryStateChanged.msg
+import time
+print('antes')
+rospy.set_param('~states/enable_commonstate_batterystatechanged', 1)
+print('depois')
+battery = CommonCommonStateBatteryStateChanged()
+print('bat exist')
+
+def bat_state (bat):
+    print ('entrou no batstate')
+    global battery
+    rospy.loginfo("%s \n", bat.percent)
+    battery.percent = bat.percent
+    print (battery.percent)
+
+rospy.init_node('Bebop_2_Battary_Porcentage')
+print('node iniciado')
+rate = rospy.Rate(5)
+print('rate definido')
+rospy.Subscriber('/bebop/states/common/CommonState/BatteryStateChanged', CommonCommonStateBatteryStateChanged, bat_state)
+print('subfeito')
+i = 0
+
+while i < 24:
+    print (battery.percent)
+    #i = i + 1
+    rate.sleep()
+    try:
+        rate.spin()
+    except Exception as e:
+        pass
+    i+=1
